9836c8b0082708669b993b8d2a87680db0181922
konakona <54853315@qq.com>
2014-08-04 21:26:36 +0800

    页面的layout

---
+++ app/views/layouts/master.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head lang="zh-cn">
+    <meta charset="UTF-8">
+    <title>Guilin Rooms</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta name="description" content="">
+    <meta name="keywords" content="Guilin Rooms,房地产,买楼,卖楼">
+    <meta property="og:title" id="fbtitle" content="Guilin Rooms">
+    <meta property="og:image" id="fbimage" content="">
+    <meta property="og:image:secure_url" id="fbimage" content="">
+
+    <!-- 新 Bootstrap 核心 CSS 文件 -->
+    <!-- <link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.2.0/css/bootstrap.min.css"> -->
+
+    <!-- 可选的Bootstrap主题文件（一般不用引入） -->
+    <!-- <link rel="stylesheet" href="http://cdn.bootcss.com/bootstrap/3.2.0/css/bootstrap-theme.min.css"> -->
+
+    <link rel="stylesheet" href="/assets/css/20140804.css" type="text/css"/>
+    <link rel="stylesheet" href="/assets/css/main.css" type="text/css"/>
+</head>
+
+<body>
+<div id="container">
+
+    <div id="title"><a href="/"><img src="/assets/images/logo.png"></a></div>
+    <div id="wrapper">
+
+        <?php echo $content; ?>
+        @yield('content')<!--无内容-->
+    </div>
+    <div id="footer">&#169; Copyrights 2014 GuilinRooms.com</div>
+</div>
+</body>
+
+<!-- jQuery文件。务必在bootstrap.min.js 之前引入 -->
+<script src="http://cdn.bootcss.com/jquery/1.11.1/jquery.min.js"></script>
+
+<!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
+<script src="http://cdn.bootcss.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+</html>
